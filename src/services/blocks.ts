16ca16a4548d60ad8a77a1470f6d7b946f107254
Srivastava57Harsh <srivastava57harsh@gmail.com>
2024-09-24 22:47:31 +0530

    :bug: Fix event indexing
    fix event indexing

--- src/services/blocks.ts
+++ src/services/blocks.ts
@@ -1,9 +1,14 @@
 import Web3 from "web3";
 import { processTransaction } from "./transactions";
+import { USDT_ABI } from "../constants/usdt";
+import { validateTransaction } from "./validator";
+import { insertTransaction } from "../database/insertion";
 
 const web3 = new Web3(
   new Web3.providers.HttpProvider("https://cloudflare-eth.com")
 );
+const USDT_CONTRACT_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+const usdtContract = new web3.eth.Contract(USDT_ABI, USDT_CONTRACT_ADDRESS);
 
 export const processBlock = async (blockNumber: number) => {
   try {
@@ -13,6 +18,33 @@ export const processBlock = async (blockNumber: number) => {
       for (const tx of block.transactions) {
         await processTransaction(tx);
       }
+    }
+
+    const events = await usdtContract.getPastEvents("allEvents", {
+      fromBlock: blockNumber,
+      toBlock: blockNumber,
+    });
+
+    for (const event of events) {
+      const transactionData = {
+        //@ts-ignore
+        transactionHash: event.transactionHash,
+        //@ts-ignore
+        blockNumber: event.blockNumber,
+        //@ts-ignore
+        from: event.from,
+        //@ts-ignore
+        to: event.to,
+        value: parseFloat(
+          //@ts-ignore
+          web3.utils.fromWei(event.value, "mwei")
+        ).toFixed(6),
+        tokenContract: USDT_CONTRACT_ADDRESS,
+      };
+
+      await validateTransaction(transactionData);
+      await insertTransaction(transactionData);
+      // console.log(`Event ${event.transactionHash} processed successfully.`);
     }
   } catch (error) {
     console.error(`Error processing block ${blockNumber}:`, error);
--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -1,44 +1,32 @@
 import { validateTransaction } from "./validator";
 import { insertTransaction } from "../database/insertion";
-
 import Web3 from "web3";
 
 const web3 = new Web3();
-
 const USDT_CONTRACT_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7";
 
 export const processTransaction = async (tx: any) => {
-  if (tx.to && tx.to.toLowerCase() === USDT_CONTRACT_ADDRESS) {
-    const methodID = tx.input.slice(0, 10); //TRAGET TRANSFER METHOD
+  try {
+    const { hash: transactionHash, blockNumber, from, to, value } = tx;
 
-    let value;
-
-    if (methodID === "0xa9059cbb") {
-      // TRANSFER METHOD
-      const rawValue = `0x${tx.input.slice(74, 138)}`;
-      value = web3.utils.hexToNumberString(rawValue);
-      const usdtValue = (parseFloat(value) / 1e6).toFixed(6);
-      value = usdtValue;
-    } else {
-      value = null;
-    }
+    const usdtValue = parseFloat(web3.utils.fromWei(value, "mwei")).toFixed(6);
 
     const transactionData = {
-      transactionHash: tx.hash,
-      blockNumber: tx.blockNumber,
-      from: tx.from,
-      to: tx.to,
-      value: value || "0",
-      tokenContract: tx.to,
-      methodID: methodID,
+      transactionHash,
+      blockNumber,
+      from,
+      to,
+      value: usdtValue,
+      tokenContract: USDT_CONTRACT_ADDRESS,
     };
 
-    try {
-      await validateTransaction(transactionData);
-      await insertTransaction(transactionData);
-      console.log(`Transaction ${tx.hash} saved`);
-    } catch (error) {
-      console.error(`Failed to process transaction ${tx.hash}:`, error);
-    }
+    await validateTransaction(transactionData);
+    await insertTransaction(transactionData);
+    console.log(`Transaction ${transactionHash} saved successfully.`);
+  } catch (error) {
+    console.error(
+      `Failed to process transaction ${tx.transactionHash}:`,
+      error
+    );
   }
 };
